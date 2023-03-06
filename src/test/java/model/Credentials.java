db86327dd5ded5b0d9cd03fafff1345e4ff66a60
Tetiana.Vasylchuk <tetiana.vasylchuk@itechart.com>
2023-03-06 09:48:19 +0100

    Create models of test data

--- src/test/java/model/Credentials.java
+++ src/test/java/model/Credentials.java
@@ -1,2 +1,28 @@
-package model;public class Credintials {
+package model;
+
+import org.checkerframework.checker.units.qual.C;
+
+public class Credentials {
+
+    private final String userName;
+    private final String password;
+
+    private Credentials(String userName, String password)
+    {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public static Credentials Create(String userName, String password){
+        return new Credentials(userName, password);
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
+
