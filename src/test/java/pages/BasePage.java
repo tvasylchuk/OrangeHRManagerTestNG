b60019b66b39506ecf6e845ecad6d2a273f149fb
Tetiana.Vasylchuk <tetiana.vasylchuk@itechart.com>
2023-03-06 09:48:48 +0100

    Create pages for basic tests

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,2 +1,32 @@
-package pages;public class BasePage {
+package pages;
+
+import framework.elements.TextBox;
+import framework.webDriverFactory.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+
+public class BasePage {
+    protected Driver browser;
+    @FindBy(xpath="//div[@id='app']")
+    WebElement appForm;
+
+    public BasePage(Driver browser) {
+        this.browser = browser;
+        PageFactory.initElements(browser.getWebDriver(), this);
+    }
+
+    public void waitTillPageLoaded()
+    {
+        try
+        {
+            browser.getWait().until((ExpectedCondition<Boolean>) input ->
+                (Boolean)appForm.isEnabled()
+            );
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,2 +1,58 @@
-package pages;public class LoginPage {
+package pages;
+
+import framework.elements.Button;
+import framework.elements.Label;
+import framework.elements.TextBox;
+import framework.webDriverFactory.Driver;
+import model.Credentials;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class LoginPage {
+    @FindBy(xpath = "//input[@name='username']")
+    private WebElement txtUserName;
+    @FindBy(xpath = "//input[@name='password']")
+    private WebElement txtPassword;
+    @FindBy(xpath = "//button[@type='submit']")
+    private WebElement btnSubmit;
+    @FindBy(xpath = "//div[@class='orangehrm-login-form']//p[text()='Invalid credentials']")
+    private WebElement lblInvalidCred;
+
+    private final Driver browser;
+
+    public LoginPage(Driver browser) {
+        this.browser = browser;
+        PageFactory.initElements(this.browser.getWebDriver(), this);
+    }
+
+    public void login(Credentials credentials)
+    {
+        TextBox txtUserNameWrapped = new TextBox(txtUserName, browser);
+        TextBox txtPasswordWrapped = new TextBox(txtPassword, browser);
+        Button btnSubmitWrapped = new Button(btnSubmit, browser);
+
+        txtUserNameWrapped.sendKeys(credentials.getUserName());
+        txtPasswordWrapped.sendKeys(credentials.getPassword());
+        btnSubmitWrapped.click();
+    }
+
+    public boolean isCredInvalid() {
+        browser.getWait().until(ExpectedConditions.visibilityOf(lblInvalidCred));
+        Label lblInvalidCredWrapper = new Label(lblInvalidCred, browser);
+        return lblInvalidCredWrapper.isDisplayed();
+    }
+
+    public  boolean isUserNameInvalid(){
+        TextBox txtUserNameWrapped = new TextBox(txtUserName, browser);
+        var className = txtUserNameWrapped.getAttribute("class");
+        return className.contains("oxd-input--error");
+    }
+
+    public  boolean isPasswordInvalid(){
+        TextBox txtPasswordWrapped = new TextBox(txtPassword, browser);
+        var className = txtPasswordWrapped.getAttribute("class");
+        return className.contains("oxd-input--error");
+    }
 }
